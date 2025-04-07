cb5d6822d95f095ee0f7893c434b52dee5909b1b
requrd <requrd1989@gmail.com>
2025-04-07 15:00:02 +0000

    fix stats module

--- config/vaapi.mjs
+++ config/vaapi.mjs
@@ -1,6 +1,6 @@
 // ref. https://github.com/plife18/docker-epgstation/blob/main/epgstation/config/enc_vaapi.js
 import { spawn } from "child_process";
-import { fs } from "fs";
+import { stat } from "node:fs/promises";
 import { getDuration } from "./getDuration.mjs";
 import { getVaapiOptions } from "./getVaapiOptions.mjs";
 
@@ -119,12 +119,13 @@ args.push(process.env.OUTPUT);
     child.kill("SIGINT");
   });
 
-  child.on("exit", (code) => {
+  child.on("exit", async (code) => {
     console.error("Exited with code: " + String(code));
-    console.error("Output: " + String(output));
-    var stat = fs.statSync(output);
-    console.error(stat.size);
-    if (stat.size < 10 * 1024) {
+    const output = process.env.OUTPUT;
+    console.error("Output: " + output);
+    const stats = await stat(output);
+    console.error(stats.size);
+    if (stats.size < 10 * 1024) {
       console.error("File site too small (< 10k). Raising error");
       throw new Error(1);
     }
