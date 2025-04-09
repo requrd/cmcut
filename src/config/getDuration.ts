590cab072dba1bb3a26e7458062f7f1ae6441dba
requrd <requrd1989@gmail.com>
2025-04-09 06:38:27 +0000

    add promise type

--- src/config/getDuration.ts
+++ src/config/getDuration.ts
@@ -8,7 +8,7 @@ const ffprobe = getenv("FFPROBE");
  * @return {Promise<int>} 動画長を返す (秒)
  */
 const getDuration = (filePath: string) => {
-  return new Promise((resolve, reject) => {
+  return new Promise<Number>((resolve, reject) => {
     execFile(
       ffprobe,
       ["-v", "0", "-show_format", "-of", "json", filePath],
