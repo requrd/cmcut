836aee0a2586e24b462b209f48edee40b00406f2
requrd <requrd1989@gmail.com>
2025-04-10 08:30:58 +0000

    fix for refactor

--- src/config/getFfmpegOptions.ts
+++ src/config/getFfmpegOptions.ts
@@ -1,6 +1,57 @@
 import { getenv } from "./getenv";
+import {
+  FfmpegOptionsPlugin,
+  getFfmpegOptions as getOptions,
+} from "./ffmpegOptionsPlugin";
 // const videoHeight = parseInt(process.env.VIDEORESOLUTION, 10);
-const isDualMono = parseInt(getenv("AUDIOCOMPONENTTYPE"), 10) == 2;
+
+const ffmpegPlugin: FfmpegOptionsPlugin = {
+  hardwareOptions: undefined,
+  videoStreamOptions: () => {
+    const codec = "libx264";
+    const videoFilter = "yadif";
+    return ["-c:v", codec, "-vf", videoFilter];
+  },
+  audioStreamOptions: (isDualMono) => {
+    return isDualMono
+      ? [
+          "-filter_complex",
+          "channelsplit[FL][FR]",
+          "-map",
+          "0:v",
+          "-map",
+          "[FL]",
+          "-map",
+          "[FR]",
+          "-metadata:s:a:0",
+          "language=jpn",
+          "-metadata:s:a:1",
+          "language=eng",
+          "-c:a",
+          "ac3",
+          "-ar",
+          "48000",
+          "-ab",
+          "256k",
+        ]
+      : ["-c:a", "aac"];
+  },
+  qualityOptions: () => {
+    const preset = "veryfast";
+    const crf = "23";
+    return [
+      "-stats",
+      "-preset",
+      preset,
+      "-aspect",
+      "16:9",
+      "-crf",
+      crf,
+      "-f",
+      "mp4",
+    ];
+  },
+};
 
 /**
  * FFmpegのオプションを作成する
@@ -9,71 +60,8 @@ const isDualMono = parseInt(getenv("AUDIOCOMPONENTTYPE"), 10) == 2;
  * @returns {string[]} - FFmpegの引数となるパラメータ
  */
 const getFfmpegOptions = (input: string | undefined = undefined) => {
-  const args: string[] = [];
-  if (input) {
-    args.push("-y", "-i", input);
-  }
-  args.push(...videoStreamOptions());
-  args.push(...audioStreamOptions(isDualMono));
-  args.push("-ignore_unknown");
-  return args.concat(...qualityOptions());
+  const isDualMono = parseInt(getenv("AUDIOCOMPONENTTYPE"), 10) == 2;
+  return getOptions(input, isDualMono, ffmpegPlugin);
 };
 
-/**
- * ビデオストリームオプション
- *
- * @returns {string[]}
- */
-const videoStreamOptions = () => {
-  const codec = "libx264"; //libx264でエンコード
-  const videoFilter = "yadif";
-  return ["-c:v", codec, "-vf", videoFilter];
-};
-/**
- * オーディオストリームオプション
- *
- * @param {boolean} isDualMono
- * @returns { string[] }
- */
-const audioStreamOptions = (isDualMono: boolean) =>
-  isDualMono
-    ? [
-        "-filter_complex",
-        "channelsplit[FL][FR]",
-        "-map",
-        "0:v",
-        "-map",
-        "[FL]",
-        "-map",
-        "[FR]",
-        "-metadata:s:a:0",
-        "language=jpn",
-        "-metadata:s:a:1",
-        "language=eng",
-        "-c:a ac3",
-        "-ar 48000",
-        "-ab 256k",
-      ]
-    : ["-c:a", "aac"];
-
-/**
- * 品質オプション
- *
- * @returns {string[]}
- */
-const qualityOptions = () => {
-  const preset = "veryfast";
-  const crf = "23";
-  return [
-    "-stats",
-    "-preset",
-    preset,
-    "-aspect",
-    "16:9",
-    "-crf",
-    crf,
-    "-f",
-    "mp4",
-  ];
-};
 export { getFfmpegOptions };
