b97535f2e6b8d612d88fb2fb3c983cdb0b790c92
requrd <requrd1989@gmail.com>
2025-04-10 14:52:55 +0000

    fix args

--- src/config/vaapi.ts
+++ src/config/vaapi.ts
@@ -9,99 +9,98 @@ const ffmpeg = getenv("FFMPEG");
 const args = getVaapiOptions(getenv("INPUT"));
 args.push(getenv("OUTPUT"));
 
-function parse(line: string) {
-  if (line.startsWith("frame")) {
-    // 想定log
-    // frame= 5159 fps= 11 q=29.0 size=  122624kB time=00:02:51.84 bitrate=5845.8kbits/s dup=19 drop=0 speed=0.372x
-    const progress: {
-      frame?: number;
-      fps?: number;
-      q?: number;
-      size?: number;
-      time?: string;
-      bitrate?: number;
-      dup?: number;
-      drop?: number;
-      speed?: number;
-    } = {};
-    const ffmpeg_reg =
-      /frame=\s*(?<frame>\d+)\sfps=\s*(?<fps>\d+(?:\.\d+)?)\sq=\s*(?<q>[+-]?\d+(?:\.\d+)?)\sL?size=\s*(?<size>\d+(?:\.\d+)?)kB\stime=\s*(?<time>\d+[:\.\d+]*)\sbitrate=\s*(?<bitrate>\d+(?:\.\d+)?)kbits\/s(?:\sdup=\s*(?<dup>\d+))?(?:\sdrop=\s*(?<drop>\d+))?\sspeed=\s*(?<speed>\d+(?:\.\d+)?)x/;
-    let ffmatch = line.match(ffmpeg_reg);
-    /**
-     * match結果
-     * [
-     *   'frame= 5159 fps= 11 q=29.0 size=  122624kB time=00:02:51.84 bitrate=5845.8kbits/s dup=19 drop=0 speed=0.372x',
-     *   '5159',
-     *   '11',
-     *   '29.0',
-     *   '122624',
-     *   '00:02:51.84',
-     *   '5845.8',
-     *   '19',
-     *   '0',
-     *   '0.372',
-     *   index: 0,
-     *   input: 'frame= 5159 fps= 11 q=29.0 size=  122624kB time=00:02:51.84 bitrate=5845.8kbits/s dup=19 drop=0 speed=0.372x    \r',
-     *   groups: [Object: null prototype] {
-     *     frame: '5159',
-     *     fps: '11',
-     *     q: '29.0',
-     *     size: '122624',
-     *     time: '00:02:51.84',
-     *     bitrate: '5845.8',
-     *     dup: '19',
-     *     drop: '0',
-     *     speed: '0.372'
-     *   }
-     * ]
-     */
+function parse(line: string, duration: number) {
+  if (!line.startsWith("frame")) return;
+  // 想定log
+  // frame= 5159 fps= 11 q=29.0 size=  122624kB time=00:02:51.84 bitrate=5845.8kbits/s dup=19 drop=0 speed=0.372x
+  const progress: {
+    frame?: number;
+    fps?: number;
+    q?: number;
+    size?: number;
+    time?: string;
+    bitrate?: number;
+    dup?: number;
+    drop?: number;
+    speed?: number;
+  } = {};
+  const ffmpeg_reg =
+    /frame=\s*(?<frame>\d+)\sfps=\s*(?<fps>\d+(?:\.\d+)?)\sq=\s*(?<q>[+-]?\d+(?:\.\d+)?)\sL?size=\s*(?<size>\d+(?:\.\d+)?)kB\stime=\s*(?<time>\d+[:\.\d+]*)\sbitrate=\s*(?<bitrate>\d+(?:\.\d+)?)kbits\/s(?:\sdup=\s*(?<dup>\d+))?(?:\sdrop=\s*(?<drop>\d+))?\sspeed=\s*(?<speed>\d+(?:\.\d+)?)x/;
+  let ffmatch = line.match(ffmpeg_reg);
+  /**
+   * match結果
+   * [
+   *   'frame= 5159 fps= 11 q=29.0 size=  122624kB time=00:02:51.84 bitrate=5845.8kbits/s dup=19 drop=0 speed=0.372x',
+   *   '5159',
+   *   '11',
+   *   '29.0',
+   *   '122624',
+   *   '00:02:51.84',
+   *   '5845.8',
+   *   '19',
+   *   '0',
+   *   '0.372',
+   *   index: 0,
+   *   input: 'frame= 5159 fps= 11 q=29.0 size=  122624kB time=00:02:51.84 bitrate=5845.8kbits/s dup=19 drop=0 speed=0.372x    \r',
+   *   groups: [Object: null prototype] {
+   *     frame: '5159',
+   *     fps: '11',
+   *     q: '29.0',
+   *     size: '122624',
+   *     time: '00:02:51.84',
+   *     bitrate: '5845.8',
+   *     dup: '19',
+   *     drop: '0',
+   *     speed: '0.372'
+   *   }
+   * ]
+   */
 
-    if (ffmatch === null) return;
-    if (ffmatch.groups) {
-      progress["frame"] = parseInt(ffmatch.groups.frame || "0");
-      progress["fps"] = parseFloat(ffmatch.groups.fps || "0");
-      progress["q"] = parseFloat(ffmatch.groups.q || "0");
-      progress["size"] = parseInt(ffmatch.groups.size || "0");
-      progress["time"] = ffmatch.groups.time || "0:0:0";
-      progress["bitrate"] = parseFloat(ffmatch.groups.bitrate || "0");
-      progress["dup"] = parseInt(ffmatch.groups.dup || "0");
-      progress["drop"] = parseInt(ffmatch.groups.drop || "0");
-      progress["speed"] = parseFloat(ffmatch.groups.speed || "0");
-    }
-
-    let current = 0;
-    const times = (progress.time || "0:0:0").split(":");
-    for (let i = 0; i < times.length; i++) {
-      if (i == 0) {
-        current += parseFloat(times[i]) * 3600;
-      } else if (i == 1) {
-        current += parseFloat(times[i]) * 60;
-      } else if (i == 2) {
-        current += parseFloat(times[i]);
-      }
-    }
-
-    // 進捗率 1.0 で 100%
-    const percent = current / duration;
-    const log = "frame= "
-      + progress.frame
-      + " fps="
-      + progress.fps
-      + " size="
-      + progress.size
-      + " time="
-      + progress.time
-      + " bitrate="
-      + progress.bitrate
-      + " drop="
-      + progress.drop
-      + " speed="
-      + progress.speed;
-
-    console.log(
-      JSON.stringify({ type: "progress", percent: percent, log: log }),
-    );
+  if (ffmatch === null) return;
+  if (ffmatch.groups) {
+    progress["frame"] = parseInt(ffmatch.groups.frame || "0");
+    progress["fps"] = parseFloat(ffmatch.groups.fps || "0");
+    progress["q"] = parseFloat(ffmatch.groups.q || "0");
+    progress["size"] = parseInt(ffmatch.groups.size || "0");
+    progress["time"] = ffmatch.groups.time || "0:0:0";
+    progress["bitrate"] = parseFloat(ffmatch.groups.bitrate || "0");
+    progress["dup"] = parseInt(ffmatch.groups.dup || "0");
+    progress["drop"] = parseInt(ffmatch.groups.drop || "0");
+    progress["speed"] = parseFloat(ffmatch.groups.speed || "0");
   }
+
+  let current = 0;
+  const times = (progress.time || "0:0:0").split(":");
+  for (let i = 0; i < times.length; i++) {
+    if (i == 0) {
+      current += parseFloat(times[i]) * 3600;
+    } else if (i == 1) {
+      current += parseFloat(times[i]) * 60;
+    } else if (i == 2) {
+      current += parseFloat(times[i]);
+    }
+  }
+
+  // 進捗率 1.0 で 100%
+  const percent = current / duration;
+  const log = "frame= "
+    + progress.frame
+    + " fps="
+    + progress.fps
+    + " size="
+    + progress.size
+    + " time="
+    + progress.time
+    + " bitrate="
+    + progress.bitrate
+    + " drop="
+    + progress.drop
+    + " speed="
+    + progress.speed;
+
+  console.log(
+    JSON.stringify({ type: "progress", percent: percent, log: log }),
+  );
 }
 
 (async () => {
@@ -120,7 +119,7 @@ function parse(line: string) {
   child.stderr.on("data", (data) => {
     let lines = String(data).split("\n");
     for (const line of lines) {
-      parse(line);
+      parse(line, duration);
     }
   });
 
