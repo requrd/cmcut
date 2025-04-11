e2475b58d5e820e73b983f84a9e3fb2cb82fef32
requrd <requrd1989@gmail.com>
2025-04-11 05:45:13 +0000

    fix to use encode

--- src/config/jlse.ts
+++ src/config/jlse.ts
@@ -4,6 +4,9 @@ import { getFfmpegOptions } from "./ffmpegOptionsPlugin";
 import { getenv } from "./getenv";
 import { softwarePlugin } from "./softwareOptions";
 
+const env = Object.create(process.env);
+env.HOME = "/root";
+
 (async () => {
   await encode(
     "jlse",
@@ -11,5 +14,6 @@ import { softwarePlugin } from "./softwareOptions";
       getFfmpegOptions(undefined, parseInt(getenv("AUDIOCOMPONENTTYPE"), 10) == 2, softwarePlugin),
       undefined,
     ),
+    env,
   );
 })();
