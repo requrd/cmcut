dbf8007247bb6e28948d704792b8f818ea46e732
requrd <requrd1989@gmail.com>
2025-04-11 13:49:37 +0000

    remove envs

--- src/config/jlse.ts
+++ src/config/jlse.ts
@@ -4,9 +4,6 @@ import { getenv } from "./getenv";
 import { getJlseArgs } from "./getJlseArgs";
 import { softwarePlugin } from "./softwareOptions";
 
-const env = Object.create(process.env);
-env.HOME = "/root";
-
 (async () => {
   await encode(
     "jlse",
@@ -14,6 +11,5 @@ env.HOME = "/root";
       getFfmpegOptions(undefined, parseInt(getenv("AUDIOCOMPONENTTYPE"), 10) == 2, softwarePlugin),
       undefined,
     ),
-    env,
   );
 })();
