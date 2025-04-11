a43c5bb74173b2fa537e3766635454bc69127e95
requrd <requrd1989@gmail.com>
2025-04-11 05:57:02 +0000

    fix to use encode

--- src/config/jlseVaapi.ts
+++ src/config/jlseVaapi.ts
@@ -1,6 +1,19 @@
-import { execJlse } from "./execJlse";
+import { encode } from "./execEncode";
 import { getFfmpegOptions } from "./ffmpegOptionsPlugin";
 import { getenv } from "./getenv";
+import { getJlseArgs } from "./getJlseArgs";
 import { vaapiOptions, vaapiPlugin } from "./vaapiOptions";
 
-execJlse(getFfmpegOptions(undefined, parseInt(getenv("AUDIOCOMPONENTTYPE"), 10) == 2, vaapiPlugin), vaapiOptions);
+const env = Object.create(process.env);
+env.HOME = "/root";
+
+(async () => {
+  await encode(
+    "jlse",
+    getJlseArgs(
+      getFfmpegOptions(undefined, parseInt(getenv("AUDIOCOMPONENTTYPE"), 10) == 2, vaapiPlugin),
+      vaapiOptions,
+    ),
+    env,
+  );
+})();
