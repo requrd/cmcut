86da9e21d7f8df29011ac83e8e896e28d7b15ed7
requrd <requrd1989@gmail.com>
2025-04-11 05:49:08 +0000

    remove unnecessary variables

--- src/config/enc.ts
+++ src/config/enc.ts
@@ -3,11 +3,12 @@ import { getFfmpegOptions } from "./ffmpegOptionsPlugin";
 import { getenv } from "./getenv";
 import { softwarePlugin } from "./softwareOptions";
 
-const command = getenv("FFMPEG");
-const args = getFfmpegOptions(getenv("INPUT"), parseInt(getenv("AUDIOCOMPONENTTYPE"), 10) == 2, softwarePlugin);
-args.push(getenv("OUTPUT"));
-
 // ここから処理開始
 (async () => {
-  await encode(command, args);
+  await encode(
+    getenv("FFMPEG"),
+    getFfmpegOptions(getenv("INPUT"), parseInt(getenv("AUDIOCOMPONENTTYPE"), 10) == 2, softwarePlugin).concat(
+      getenv("OUTPUT"),
+    ),
+  );
 })();
